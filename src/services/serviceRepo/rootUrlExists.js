35d6cea1a442bdf499c24daa616a6393580e38ca
maumasi <liu.maumasi@gmail.com>
2016-10-14 07:45:50 -0400

    test

--- src/services/serviceRepo/rootUrlExists.js
+++ src/services/serviceRepo/rootUrlExists.js
@@ -6,37 +6,12 @@ module.exports = (url, callback) => {
     url: url.originalURL,
   };
 
-  // function evaluateUrl(error, response) {
-  //   let isActive;
-  //   const status = response.statusCode;
-  //   if (!error && status <= 308) {
-  //     isActive = true;
-  //   } else {
-  //     log(error, __filename,
-  //       'Service: rootUrlExists',
-  //       `URL is unreachable with status code of: ${status}`);
-  //     isActive = false;
-  //   }
-  //   callback(isActive);
-  // }
-
   log(null, __filename,
     'Service: rootUrlExists',
     'rootUrlExists executed');
 
-  request(options, (err, response, body) => {
-    let isActive;
-    // const status = response.statusCode;
-    log(null, __filename, body);
-    // log(null, __filename, status);
-    if (body) {
-      isActive = true;
-    } else {
-      log(null, __filename,
-        'Service: rootUrlExists',
-        'URL is unreachable');
-      isActive = false;
-    }
+  request(options, () => {
+    const isActive = true;
     callback(isActive);
   });
 };
